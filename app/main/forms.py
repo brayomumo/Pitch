eb0c554a1584f5f9274579d7b93e403c521bf4b6
brayomumo <brayomumo5@gmail.com>
2019-08-03 13:16:31 +0300

    add new pitch form

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,3 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField,SubmitField,RadioField
 from wtforms.validators import Required
 
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title', validators=[Required()])
+    body = TextAreaField('Enter your comment here', validators=[Required()])
+    author = StringField('Author', validators=[Required()])
+    category = RadioField('Pick Category',choices=[('business', 'business'),('wired', 'wired')],validators=[Required()])
+    submit = SubmitField('Submit')
