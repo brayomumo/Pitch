43a6d6ae1e4d81708c6062663bf5d3fd859dd685
brayomumo <brayomumo5@gmail.com>
2019-08-03 13:47:37 +0300

    configurations

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-class Comfig:
+class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://smoucha:mumo@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
@@ -8,6 +8,14 @@ class Comfig:
     def init_app(app):
         pass
 
-class ProdConfig(config):
+class ProdConfig(Config):
     pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
 
--- manage.py
+++ manage.py
@@ -9,10 +9,9 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
 migrate = Migrate(app,db)
-migrate.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User, Review = Review)
-
+    return dict(app = app, db = db, User = User)
 if __name__=='__main__':
     manager.run()
